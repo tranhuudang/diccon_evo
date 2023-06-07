76a91812c469194d6d9a94e63ec8808056fa9384
I-DangTran <tranhuudang148@gmail.com>
2023-06-07 23:39:49 +0700

    fix bug that hide title bar on reading page

--- lib/views/article_page.dart
+++ lib/views/article_page.dart
@@ -4,6 +4,9 @@ import '../components/clickable_words.dart';
 import '../components/header.dart';
 import '../global.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+
+import '../viewModels/platform_check.dart';
 
 class ArticlePageView extends StatefulWidget {
   final String title;
@@ -39,93 +42,146 @@ class _ArticlePageViewState extends State<ArticlePageView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          widget.title,
-          style: const TextStyle(color: Colors.black),
-        ),
-        elevation: 0,
-        iconTheme: const IconThemeData(
-          color: Colors.black,
-        ),
-      ),
+      appBar: PlatformCheck.isMobile()
+          ? AppBar(
+              backgroundColor: Colors.white,
+              title: Text(
+                widget.title,
+                style: const TextStyle(color: Colors.black),
+              ),
+              elevation: 0.5,
+              iconTheme: const IconThemeData(
+                color: Colors.black,
+              ),
+            )
+          : PreferredSize(
+              preferredSize: const Size.fromHeight(50.0),
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white, // Windows 11 title bar color
+                  border: Border(
+                    bottom: BorderSide(color: Colors.black12, width: 0.7),
+                  ),
+                ),
+                child: MoveWindow(
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      IconButton(onPressed: (){
+                        Navigator.pop(context);
+                      }, icon: const Icon(Icons.arrow_back)),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Text(
+                        widget.title,
+                        style: const TextStyle(color: Colors.black),
+                      ),
+                      const Spacer(),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Row(
+                          children: [
+                            MinimizeWindowButton(
+                              colors: Global.buttonColors,
+                            ),
+                            MaximizeWindowButton(
+                              colors: Global.buttonColors,
+                            ),
+                            CloseWindowButton(
+                              colors: Global.closeButtonColors,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
-          
           children: [
             /// Image for the article
             Flexible(
               child: SingleChildScrollView(
                 child: Column(
-                children: [
-                CachedNetworkImage(
-                    placeholder: (context, url) => const LinearProgressIndicator(
-                          backgroundColor: Colors.black45,
-                          color: Colors.black54,
-                        ),
-                    imageUrl: widget.imageUrl,
-
-                    fit: BoxFit.none,
-                    errorWidget: (context, String exception, dynamic stackTrace) {
-                      return Container(
-                        width: 100.0,
-                        height: 100.0,
-                        color: Colors.grey, // Display a placeholder color or image
-                        child: Center(
-                          child: Text('No Image'),
-                        ),
-                      );
-                    }),
-                SizedBox(height: 16,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: widget.content.split('\n').map((paragraph) {
-                    return Column(
+                  children: [
+                    CachedNetworkImage(
+                        placeholder: (context, url) =>
+                            const LinearProgressIndicator(
+                              backgroundColor: Colors.black45,
+                              color: Colors.black54,
+                            ),
+                        imageUrl: widget.imageUrl,
+                        fit: BoxFit.none,
+                        errorWidget:
+                            (context, String exception, dynamic stackTrace) {
+                          return Container(
+                            width: 100.0,
+                            height: 100.0,
+                            color: Colors
+                                .grey, // Display a placeholder color or image
+                            child: const Center(
+                              child: Text('No Image'),
+                            ),
+                          );
+                        }),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        paragraph.isNotEmpty
-                            ? ClickableWords(
-                                text: paragraph,
-                                fontSize: Global.readingFontSize,
-                                textColor: Colors.black,
-                                onWordTap: (value) {
-                                  setState(() {
-                                    isTranslating = true;
-                                  });
-                                  translate(value).then((value) {
-                                    setState(() {
-                                      translatedWord = value.text;
-                                      isTranslating = false;
-                                    });
-                                  });
-                                })
-                            : Container(),
-                        SizedBox(
-                          height: 5,
-                        )
-                      ],
-                    );
-                  }).toList(),
+                      children: widget.content.split('\n').map((paragraph) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            paragraph.isNotEmpty
+                                ? ClickableWords(
+                                    text: paragraph,
+                                    fontSize: Global.readingFontSize,
+                                    textColor: Colors.black,
+                                    onWordTap: (value) {
+                                      setState(() {
+                                        isTranslating = true;
+                                      });
+                                      translate(value).then((value) {
+                                        setState(() {
+                                          translatedWord = value.text;
+                                          isTranslating = false;
+                                        });
+                                      });
+                                    })
+                                : Container(),
+                            const SizedBox(
+                              height: 5,
+                            )
+                          ],
+                        );
+                      }).toList(),
+                    ),
+                  ],
                 ),
-                ],),
               ),
             ),
+
             /// Bottom box for translation
             Container(
-              padding: EdgeInsets.all(8),
+              padding: const EdgeInsets.all(8),
               height: 50,
-              decoration: BoxDecoration(),
+              decoration: const BoxDecoration(),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   const Icon(Icons.translate),
-                  SizedBox(
+                  const SizedBox(
                     width: 8,
                   ),
                   isTranslating
-                      ? Container(
+                      ? const SizedBox(
                           height: 8,
                           width: 40,
                           child: LinearProgressIndicator())
