cd4f91ce9bb94e42a9da79819ca4424151a189cb
I-DangTran <tranhuudang148@gmail.com>
2023-06-08 10:23:39 +0700

    change cursor in article list

--- lib/views/article_list.dart
+++ lib/views/article_list.dart
@@ -118,111 +118,114 @@ class _ArticleListViewState extends State<ArticleListView> {
             ),
             itemBuilder: (context, index) {
               return GridTile(
-                child: GestureDetector(
-                  onTap: () {
-                    // Handle tap on article
-                    // For example, navigate to article details page
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ArticlePageView(
-                                  content: _listArticles[index].content,
-                                  title: _listArticles[index].title,
-                                  imageUrl: Global
-                                          .defaultArticleList[index].imageUrl ??
-                                      "",
-                                )));
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Container(
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      width: 300,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2.0,
-                              ),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(13),
-                              child: CachedNetworkImage(
-                                placeholder: (context, url) =>
-                                    const LinearProgressIndicator(
-                                  backgroundColor: Colors.black45,
-                                  color: Colors.black54,
+                child: MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: () {
+                      // Handle tap on article
+                      // For example, navigate to article details page
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ArticlePageView(
+                                    content: _listArticles[index].content,
+                                    title: _listArticles[index].title,
+                                    imageUrl: Global
+                                            .defaultArticleList[index].imageUrl ??
+                                        "",
+                                  )));
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        width: 300,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2.0,
                                 ),
-                                imageUrl: _listArticles[index].imageUrl ?? '',
-                                height: 100.0,
-                                width: 100.0,
-                                fit: BoxFit.cover,
-                                errorWidget: (context, String exception,
-                                    dynamic stackTrace) {
-                                  return Container(
-                                    width: 100.0,
-                                    height: 100.0,
-                                    color: Colors
-                                        .grey, // Display a placeholder color or image
-                                    child: const Center(
-                                      child: Text('No Image'),
-                                    ),
-                                  );
-                                },
                               ),
-                            ),
-                          ),
-                          const SizedBox(width: 8.0),
-                          Flexible(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: SingleChildScrollView(
-                                physics: const NeverScrollableScrollPhysics(),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    SingleChildScrollView(
-                                      scrollDirection: Axis.horizontal,
-                                      child: Row(
-                                        children: [
-                                          LevelIcon(
-                                            level: _listArticles[index].level ??
-                                                Level.intermediate,
-                                          ),
-                                          const SizedBox(
-                                            width: 4,
-                                          ),
-                                          Text(
-                                            _listArticles[index].title,
-                                            textAlign: TextAlign.center,
-                                            style: const TextStyle(
-                                                fontSize: 16.0,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                        ],
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(13),
+                                child: CachedNetworkImage(
+                                  placeholder: (context, url) =>
+                                      const LinearProgressIndicator(
+                                    backgroundColor: Colors.black45,
+                                    color: Colors.black54,
+                                  ),
+                                  imageUrl: _listArticles[index].imageUrl ?? '',
+                                  height: 100.0,
+                                  width: 100.0,
+                                  fit: BoxFit.cover,
+                                  errorWidget: (context, String exception,
+                                      dynamic stackTrace) {
+                                    return Container(
+                                      width: 100.0,
+                                      height: 100.0,
+                                      color: Colors
+                                          .grey, // Display a placeholder color or image
+                                      child: const Center(
+                                        child: Text('No Image'),
                                       ),
-                                    ),
-                                    Text(
-                                      _listArticles[index].shortDescription,
-                                      textAlign: TextAlign.justify,
-                                      style: TextStyle(
-                                          fontSize: 12.0,
-                                          color: Colors.black45),
-                                    ),
-                                  ],
+                                    );
+                                  },
                                 ),
                               ),
                             ),
-                          ),
-                        ],
+                            const SizedBox(width: 8.0),
+                            Flexible(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: SingleChildScrollView(
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      SingleChildScrollView(
+                                        scrollDirection: Axis.horizontal,
+                                        child: Row(
+                                          children: [
+                                            LevelIcon(
+                                              level: _listArticles[index].level ??
+                                                  Level.intermediate,
+                                            ),
+                                            const SizedBox(
+                                              width: 4,
+                                            ),
+                                            Text(
+                                              _listArticles[index].title,
+                                              textAlign: TextAlign.center,
+                                              style: const TextStyle(
+                                                  fontSize: 16.0,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Text(
+                                        _listArticles[index].shortDescription,
+                                        textAlign: TextAlign.justify,
+                                        style: TextStyle(
+                                            fontSize: 12.0,
+                                            color: Colors.black45),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
