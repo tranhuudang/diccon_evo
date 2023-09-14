d822bbb2c384145eb4016c60fd5b536dd25731ea
Tran Huu Dang <96557038+tranhuudang@users.noreply.github.com>
2023-09-14 22:21:28 +0700

    Update home_menu_button.dart

--- lib/screens/home/ui/components/home_menu_button.dart
+++ lib/screens/home/ui/components/home_menu_button.dart
@@ -1,5 +1,8 @@
 import 'package:diccon_evo/extensions/i18n.dart';
+import 'package:diccon_evo/extensions/target_platform.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../settings/ui/settings.dart';
 class HomeMenuButton extends StatelessWidget {
@@ -28,12 +31,49 @@ class HomeMenuButton extends StatelessWidget {
           ),
           itemBuilder: (context) => [
             PopupMenuItem(
-              child: Text("Settings".i18n),
+              child: Row(
+                children: [
+                  Icon(Icons.settings,),
+                  SizedBox(width: 8,),
+                  Text("Settings".i18n),
+                ],
+              ),
               onTap: () {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
                         builder: (context) => const SettingsView()));
+              },
+            ),
+            const PopupMenuItem(
+              enabled: false,
+              height: 0,
+              child: Divider(),
+            ),
+            PopupMenuItem(
+              child: Row(
+                children: [
+                  Icon(Icons.feedback_outlined),
+                  SizedBox(width: 8,),
+                  Text("Feedbacks".i18n),
+                ],
+              ),
+              onTap: () async {
+                if (defaultTargetPlatform.isMobile()) {
+                  final Uri url = Uri.parse(
+                      'https://play.google.com/store/apps/details?id=com.zeroboy.diccon_evo');
+                  if (!await launchUrl(url,
+                  mode: LaunchMode.externalApplication)) {
+                throw Exception('Could not launch $url');
+                }
+                } else {
+                final Uri url = Uri.parse(
+                'https://apps.microsoft.com/store/detail/diccon-evo/9NPF4HBMNG5D');
+                if (!await launchUrl(url,
+                mode: LaunchMode.externalApplication)) {
+                throw Exception('Could not launch $url');
+                }
+                }
               },
             ),
           ],
