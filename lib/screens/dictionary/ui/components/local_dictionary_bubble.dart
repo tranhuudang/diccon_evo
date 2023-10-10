c4e740d93d0c52515fe810baaf4286b07faa00f3
Tran Huu Dang <96557038+tranhuudang@users.noreply.github.com>
2023-10-10 16:56:04 +0700

    Update local_dictionary_bubble.dart

--- lib/screens/dictionary/ui/components/local_dictionary_bubble.dart
+++ lib/screens/dictionary/ui/components/local_dictionary_bubble.dart
@@ -86,8 +86,8 @@ class _LocalDictionaryBubbleState extends State<LocalDictionaryBubble> {
                           chatListBloc.add(AddLocalTranslation(
                               providedWord: currentWord));
                         },
-                        highlightColor: Colors.white,
-                        subColor: Colors.white70,
+                        highlightColor: Theme.of(context).textTheme.bodyLarge?.color,
+                        subColor: Theme.of(context).textTheme.bodyMedium?.color,
                       ),
                     ],
                   ),
