6750d50df6e0155ba7cbca64ad8c05dfb71554fc
Tran Huu Dang <96557038+tranhuudang@users.noreply.github.com>
2023-10-17 22:36:56 +0700

    add waves animation to conversation button

--- lib/screens/home/ui/components/to_conversation_button.dart
+++ lib/screens/home/ui/components/to_conversation_button.dart
@@ -3,8 +3,10 @@ import 'package:diccon_evo/data/helpers/feedback_helper.dart';
 import 'package:diccon_evo/extensions/i18n.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:wave/config.dart';
 import '../../../../config/route_constants.dart';
 import 'feature_button.dart';
+import 'package:wave/wave.dart';
 
 class ToConversationButton extends StatelessWidget {
   const ToConversationButton({
@@ -16,46 +18,61 @@ class ToConversationButton extends StatelessWidget {
     return FeatureButton(
       borderColor: Theme.of(context).scaffoldBackgroundColor,
       padding: EdgeInsets.zero,
-      image: const DecorationImage(
-        fit: BoxFit.cover,
-        image: AssetImage("assets/theme/theme-1-1.png"),
-      ),
       onTap: () {
         FeedbackHelper.showFeedbackBottomSheet(context);
         context.pushNamed(RouterConstants.conversation);
       },
       child: ClipRRect(
         borderRadius: BorderRadius.circular(30),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 5),
-          child: Container(
-            color: Theme.of(context).colorScheme.primary.withOpacity(0.9),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.chat,
-                      size: 30, color: Theme.of(context).colorScheme.onPrimary),
-                  const SizedBox(height: 4),
-                  Text(
-                    "Ask me anything".i18n,
-                    style: TextStyle(
-                        fontSize: 12,
+        child: Stack(
+          children: [
+            Container(
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.9),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.chat,
+                        size: 30,
                         color: Theme.of(context).colorScheme.onPrimary),
-                  ),
-                  Text(
-                    "Mr. Know It All",
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.onPrimary),
-                  ),
-                  const SizedBox(height: 16),
-                ],
+                    const SizedBox(height: 4),
+                    Text(
+                      "Ask me anything".i18n,
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Theme.of(context).colorScheme.onPrimary),
+                    ),
+                    Text(
+                      "Mr. Know It All",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.onPrimary),
+                    ),
+                    const SizedBox(height: 16),
+                  ],
+                ),
               ),
             ),
-          ),
+            Opacity(
+              opacity: .5,
+              child: WaveWidget(
+                config: CustomConfig(
+                  colors: [
+                    Colors.white70,
+                    Colors.white54,
+                    Colors.white30,
+                    Colors.white24,
+                  ],
+                  durations: [32000, 21000, 18000, 5000],
+                  heightPercentages: [0.75, 0.66, 0.68, 0.71],
+                ),
+                size: const Size(double.infinity, double.infinity),
+                waveAmplitude: 0,
+              ),
+            ),
+          ],
         ),
       ),
     );
