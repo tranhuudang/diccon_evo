d649280b1a52cd5b83216e13437d062527b37f23
Tran Huu Dang <96557038+tranhuudang@users.noreply.github.com>
2023-11-10 06:08:39 -0800

    little change

--- lib/src/features/dictionary/data/bloc/bloc.dart
+++ lib/src/features/dictionary/data/bloc/bloc.dart
@@ -1,3 +1,4 @@
 export 'dictionary_bloc.dart';
 export 'text_recognizer_bloc.dart';
 export 'word_history_bloc.dart';
+export 'dictionary_preferences_bloc.dart';
--- lib/src/features/dictionary/presentation/ui/screens/dictionary_preferences.dart
+++ lib/src/features/dictionary/presentation/ui/screens/dictionary_preferences.dart
@@ -1,4 +1,3 @@
-import 'package:diccon_evo/src/features/dictionary/data/bloc/dictionary_preferences_bloc.dart';
 import 'package:diccon_evo/src/features/features.dart';
 import 'package:diccon_evo/src/common/common.dart';
 import 'package:flutter/material.dart';
