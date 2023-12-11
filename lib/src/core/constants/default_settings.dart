cceb48889598ed074db0c417b1eea0a6f391c618
Tran Huu Dang <96557038+tranhuudang@users.noreply.github.com>
2023-12-11 16:13:32 +0000

    Update default_settings.dart

--- lib/src/core/constants/default_settings.dart
+++ lib/src/core/constants/default_settings.dart
@@ -5,7 +5,7 @@ import '../../core/core.dart';
 class DefaultSettings{
   static const String appName = "Diccon";
   static const int version = 570;
-  static const Size defaultWindowsSize = Size(400, 700);
+  static const Size defaultWindowsSize = Size(620, 670);
   static const Size minWindowsSize = Size(620, 600);
   static const Size maxWindowsSize = Size(5000, 10000);
   static const double overflowHeight = 745;
