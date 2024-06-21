31b350cdfc0e958364967f66a64d4c7bbb680611
Tran Huu Dang <96557038+tranhuudang@users.noreply.github.com>
2024-06-21 15:04:07 +0700

    fix: add more stories

--- lib/src/presentation/dialogue/utils.dart
+++ lib/src/presentation/dialogue/utils.dart
@@ -1,8 +1,20 @@
 import 'package:diccon_evo/src/core/enum/sex.dart';
 
-Sex sexDetectorByName(String name){
-  final femaleNameList = ['Rachel','Sarah','Emily','Emma','Rachel'];
-  final maleNameList = ['David','Mark','Alex','Michael'];
+Sex sexDetectorByName(String name) {
+  final femaleNameList = ['Rachel', 'Sarah', 'Emily', 'Emma', 'Rachel', 'Lisa'];
+  final maleNameList = [
+    'David',
+    'Mark',
+    'Alex',
+    'Michael',
+    'Chris',
+    'Jake',
+    'John',
+    'Dr. Miller',
+    'James',
+    'Dr. Patel',
+    'Dr. Thompson'
+  ];
   if (femaleNameList.contains(name)) return Sex.women;
   if (maleNameList.contains(name)) return Sex.men;
   return Sex.men;
